db325615a8987b8798f73ded255f0e30510d8e68
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-09-09 22:10:03 +0300

    Install Django REST Framework

--- pavlutin/settings/base.py
+++ pavlutin/settings/base.py
@@ -12,6 +12,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
