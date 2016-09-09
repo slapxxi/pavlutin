fb44aaab4c3a461b668c25d57877d90ecd67e9e6
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-09-09 21:55:27 +0300

    Configure urls to serve static files

--- pavlutin/urls.py
+++ pavlutin/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import urlpatterns as static_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-] + static_urlpatterns
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
