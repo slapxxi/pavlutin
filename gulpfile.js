7f6910b052a17d95d613acbae5ab6ccede7a5ba6
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-09-12 01:56:01 +0300

    Minify CSS files via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
 
+
 gulp.task('css', function() {
   var postcss = require('gulp-postcss');
   var sourcemaps = require('gulp-sourcemaps');
+  var concatcss = require('gulp-concat-css');
+
+  var postcssProcessors = [
+    require('autoprefixer'),
+    require('precss')({
+      import: {extension: 'scss'}
+    }),
+    require('cssnano')
+  ];
 
   return gulp.src('src/**/*.css')
     .pipe(sourcemaps.init())
-    .pipe(postcss([require('autoprefixer'), require('precss')]))
+    .pipe(postcss(postcssProcessors))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('static/'));
 });
+
+
+gulp.task('watch:css', function() {
+  gulp.watch(['src/**/*.css', 'src/**/*.scss'], ['css']);
+})
