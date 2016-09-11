af5829b88cf14cf8c57dda00bec47d72dfff4679
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-09-12 00:53:40 +0300

    Add Gulp task to parse CSS using PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+gulp.task('css', function() {
+  var postcss = require('gulp-postcss');
+  var sourcemaps = require('gulp-sourcemaps');
+
+  return gulp.src('src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([require('autoprefixer'), require('precss')]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('static/'));
+});
