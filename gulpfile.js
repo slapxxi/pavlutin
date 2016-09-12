5de38db4013223844533c1de0a5fcf18f9c6e75e
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-09-12 20:42:44 +0300

    Specify 'clean' task as a dependency for build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const webpack = require('gulp-webpack');
 const del = require('del');
 
+const ENV = environment(process.env.NODE_ENV);
+
 
 gulp.task('default', ['build'])
 
@@ -9,14 +11,9 @@ gulp.task('watch', () => {
   gulp.watch('src/**/*.{js,css,scss}', ['build'])
 });
 
-gulp.task('build', ['clean', 'build:css', 'build:js']);
+gulp.task('build', ['build:css', 'build:js']);
 
-gulp.task('clean', (cb) => {
-  del(['static/css', 'static/js']);
-  return cb(null);
-});
-
-gulp.task('build:css', () => {
+gulp.task('build:css', ['clean'], () => {
   const postcss = require('gulp-postcss');
   const sourcemaps = require('gulp-sourcemaps');
   const concatcss = require('gulp-concat-css');
@@ -36,8 +33,16 @@ gulp.task('build:css', () => {
     .pipe(gulp.dest('static'));
 });
 
-gulp.task('build:js', () => {
+gulp.task('build:js', ['clean'], () => {
   return gulp.src('src/js/index.js')
     .pipe(webpack(require('./webpack.config')))
     .pipe(gulp.dest('static/js'))
 })
+
+gulp.task('clean', () => {
+  return del(['static/css', 'static/js']);
+});
+
+function environment(env) {
+  return env || 'development';
+}
