9a46232710a75a384d5f8f79901fea4fee1b0d69
Slava Pavlutin <sl.pavlutin@gmail.com>
2016-10-08 19:26:42 +0300

    Add menu button

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,11 @@ $(document).ready(() => {
 
   const $header = $('.l-header');
   const $nav = $header.find('> .nav');
-  const $menu = $('<a href="#" class="button">Menu</a>');
+  const $menu = $('<button class="btn-menu"></button>');
 
   $menu.on('click', function(e) {
-    $(this).text(toggleText);
+    // $(this).text(toggleText);
+    $(this).toggleClass('btn-menu_active');
     $header.toggleClass('l-header_fullscreen');
     $header.siblings().toggle();
     return false;
